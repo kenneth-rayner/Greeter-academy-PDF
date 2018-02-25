b5514314a5fc0f4c4f40abb7fb7102a916a8f64e
Kevin Coatsworth <kevin.coatsworth@digital.hmrc.gov.uk>
2018-02-25 22:59:35 +0000

    add age request and refactor speak function.

--- src/main/scala/app/GreeterApplication.scala
+++ src/main/scala/app/GreeterApplication.scala
@@ -6,21 +6,23 @@ object Prompt {
   def ask(message : String) = StdIn.readLine(message)
 }
 
-class Person(name : String) {
+class Person(name : String, age : Int) {
 
+  private val years : String = if(age > 1) "years" else "year"
   def speak() : String = {
     if(name == "adam") {
       s"You don't get a hello!"
     } else {
-      s"Hello $name"
+      s"Hello $name, you are $age $years old"
     }
   }
 }
 
 object GreeterApplication extends App {
-
   val name = Prompt.ask("What is your name? ")
-  val person = new Person(name)
+  val age = Prompt.ask("How old are you? ")
+
+  val person = new Person(name, age.toInt)
   println(person.speak())
 
 }
