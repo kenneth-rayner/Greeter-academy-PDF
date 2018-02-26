61efef76ae95db3241d5087ac020169b651375d3
Kevin Coatsworth <kevin.coatsworth@digital.hmrc.gov.uk>
2018-02-26 21:42:35 +0000

    Add Bank account and Savings Account classes.

--- src/main/scala/app/GreeterApplication.scala
+++ src/main/scala/app/GreeterApplication.scala
@@ -6,14 +6,55 @@ object Prompt {
   def ask(message : String) = StdIn.readLine(message)
 }
 
-class Person(name : String, age : Int) {
+abstract class BankAccount(accountNumber : String, val balance : Double) {
+  def withdraw(amount : Double) : BankAccount
+  def deposit(amount : Double) : BankAccount
+}
 
+final class SavingsAccount(accountNumber : String, balance : Double) extends BankAccount(accountNumber, balance) {
+  override def withdraw(amount : Double) : BankAccount = {
+    if((balance - amount) < 0) {
+
+      println(s"You have insufficient funds")
+      this
+    } else {
+
+      val deducted = balance - amount
+      println(s"Balance after deductions: $deducted")
+      new SavingsAccount(accountNumber, deducted)
+    }
+  }
+  override def deposit(amount : Double) : BankAccount = {
+    new SavingsAccount(accountNumber, balance + amount)
+  }
+
+}
+
+final class CashISASavingsAccount(accountNumber : String, balance : Double) extends BankAccount(accountNumber, balance) {
+
+  override def withdraw(amount: Double): BankAccount = {
+
+    println(s"You can't withdraw yet, your money is locked in for 3 years!!!")
+    this
+  }
+
+  override def deposit(amount: Double): BankAccount = {
+
+    new CashISASavingsAccount(accountNumber, balance + amount)
+
+  }
+
+}
+
+class Person(name : String, age : Int, private val bankAccount: BankAccount) {
+
+  def this(name: String, age : Int) = this(name, age, new SavingsAccount("12345", 0.00))
   private val years : String = if(age > 1) "years" else "year"
   def speak() : String = {
     if(name == "adam") {
       s"You don't get a hello!"
     } else {
-      s"Hello $name, you are $age $years old"
+      s"Hello $name, you are $age $years old. \nYou have ${bankAccount.balance} in your account."
     }
   }
 }
