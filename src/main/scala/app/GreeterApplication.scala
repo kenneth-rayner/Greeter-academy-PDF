5ed99121db98636d406a3926d1e3d4e5270ecc5e
Kevin Coatsworth <kevin.coatsworth@digital.hmrc.gov.uk>
2018-03-02 12:29:55 +0000

    Implement multiple bank accounts.

--- src/main/scala/app/GreeterApplication.scala
+++ src/main/scala/app/GreeterApplication.scala
@@ -11,12 +11,12 @@ object GreeterApplication extends App {
   val cashisa = new CashISASavingsAccount("45676", 0.0, 1000.00)
   val deposited = cashisa.deposit(1000.00)
   val withdrawn = deposited.withdraw(200.00)
-  val person = new Person(name, age.toInt, withdrawn)
+  val person = new Person(name, age.toInt, List(withdrawn))
 
   val normalAccount = new CashISASavingsAccount("12334", 100.00)
   val loyalAccountDeposited = normalAccount.deposit(300.00)
 
-  val loyal = new Person("Loyal customer", 22, loyalAccountDeposited)
+  val loyal = new Person("Loyal customer", 22, List(loyalAccountDeposited))
   //Prompt.reply(loyal.speak())
   val p1 = new Person(name, age.toInt)
   Prompt.reply(p1.speak())
--- src/main/scala/app/models/Person.scala
+++ src/main/scala/app/models/Person.scala
@@ -1,15 +1,15 @@
 package app.models
 
-class Person(name : String, age : Int, private val bankAccount: BankAccount) {
+class Person(name : String, age : Int, val bankAccounts: Seq[BankAccount] = Nil) {
 
-  def this(name: String, age : Int) = this(name, age, new SavingsAccount("12345", 0.00))
+  def this(name: String, age : Int) = this(name, age, List(new SavingsAccount("12345", 0.00)))
   private val years : String = if(age > 1) "years" else "year"
   private val excluded = List("adam", "daniel")
   def speak() : String = {
     if(excluded.contains(name)) {
       s"You don't get a hello!"
     } else {
-      s"Hello $name, you are $age $years old. \nYour account details are: $bankAccount"
+      s"Hello $name, you are $age $years old. \nYour account details are: $bankAccounts"
     }
   }
 }
